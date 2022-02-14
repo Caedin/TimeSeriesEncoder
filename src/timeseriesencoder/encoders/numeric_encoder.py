fixed tests, added precision and functional optional parameters

src/timeseriesencoder/encoders/numeric_encoder.py
@@ -149,7 +149,11 @@ class NumericEncoder:
         vector = new_vector.astype(int)
         for i in range(self.encoding_depth - 1):
             offset = (self.encoding_size ** (self.encoding_depth - i - 1))
-            vector[:, i] = vector[:, i] * offset
+            try:
+                vector[:, i] = vector[:, i] * offset
+            except OverflowError:
+                vector = vector.astype(object)
+                vector[:, i] = vector[:, i] * offset
         vector = np.sum(vector, axis=1)
 
         # Adjust for signage

src/timeseriesencoder/encoders/time_series_encoder.py
@@ -517,7 +517,10 @@ class CSVEncoder(TimeSeriesEncoder):
         return np.asarray(words).reshape(-1, 1)
 
     @staticmethod
-    def encode_csv(csv, time_column, key_columns, sort_values = True, encoding_size = 64, gzip=False, functional_compression=False):
+    def encode_csv(csv, time_column, key_columns, sort_values = True, encoding_size = 64, gzip=False, functional_compression=True, maximum_precision=6):
+        global MAX_FLOATING_PRECISION
+        MAX_FLOATING_PRECISION = maximum_precision
+        
         df = pd.read_csv(StringIO(csv))
         df = df.dropna()
 